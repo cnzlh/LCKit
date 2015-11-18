0d78c35b965bb5325a343e95d82a88494eb90b7f
linhua <cnzlh@163.com>
2015-11-18 14:11:13 +0800

    给UIImage类别添加新方法

--- Pod/Classes/CommonTools/Category/UIImage+LCExtension.h
+++ Pod/Classes/CommonTools/Category/UIImage+LCExtension.h
@@ -24,4 +24,56 @@
  */
 -(UIImage*)getSubImage:(CGRect)rect;
 
+/**
+ *  缩放图片到指定大小
+ *
+ */
++ (UIImage *)thumbnailWithImage:(UIImage *)image size:(CGSize)newSize;
+
+/**
+ *  保持原来的长宽比，生成一个缩略图
+ *
+ */
++ (UIImage *)thumbnailWithImageWithoutScale:(UIImage *)image size:(CGSize)asize;
+
+/**
+ *  保持图像比例不变的同时调整其大小。需要根据图像的长宽比在其四周留出一些额外区域，用透明像素为图像消关。
+ *
+ */
++ (UIImage *) thumbnailWithImage:(UIImage *)image fitInSize:(CGSize) viewsize;
+
+/**
+ *  抽取部分图像来匹配可用空间。并裁剪掉落在像素区外的任何元素。
+ *
+ *  @return 返回居中的缩略图
+ */
++ (UIImage *) thumbnailWithImage:(UIImage *)image centerInSize:(CGSize) viewsize;
+
+/**
+ *  通过匹配可用空间的长度和宽度来填充图像。每个像素都被使用，但是图像将水平或垂直裁剪
+ *
+ *  @return 返回填充的缩略图
+ */
++ (UIImage *) thumbnailWithImage:(UIImage *)image fillInSize:(CGSize) viewsize;
+
+/**
+ *	@brief	按比例缩放图片
+ *
+ *	@param 	scaleRatio 	缩放比率
+ */
+- (UIImage *)scaleWithRatio:(CGFloat)scaleRatio;
+
+/**
+ *	@brief	image转换为String,可做为参数传递
+ *
+ */
+- (NSString *)image2String;
+
+/**
+ *	@brief	高斯模糊
+ *
+ */
+- (UIImage *)applyBlurWithRadius:(CGFloat)blurRadius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor maskImage:(UIImage *)maskImage;
+
+
 @end
--- Pod/Classes/CommonTools/Category/UIImage+LCExtension.m
+++ Pod/Classes/CommonTools/Category/UIImage+LCExtension.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIImage+LCExtension.h"
+#import <Accelerate/Accelerate.h>
 
 @implementation UIImage (LCExtension)
 
@@ -27,13 +28,308 @@
     CGImageRef subImageRef = CGImageCreateWithImageInRect(self.CGImage, rect);
     CGRect smallBounds = CGRectMake(0, 0, CGImageGetWidth(subImageRef), CGImageGetHeight(subImageRef));
     
-    UIGraphicsBeginImageContext(smallBounds.size);
+    CGFloat scale = [[UIScreen mainScreen] scale];
+    UIGraphicsBeginImageContextWithOptions(smallBounds.size, NO, scale);
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextDrawImage(context, smallBounds, subImageRef);
     UIImage* smallImage = [UIImage imageWithCGImage:subImageRef];
     UIGraphicsEndImageContext();
     
     return smallImage;
+}
+
++ (UIImage *)thumbnailWithImage:(UIImage *)image size:(CGSize)newSize
+{
+    UIImage *newimage;
+    if (nil == image)
+    {
+        newimage = nil;
+    }
+    else
+    {
+        CGFloat scale = [[UIScreen mainScreen] scale];
+        UIGraphicsBeginImageContextWithOptions(newSize, NO, scale);
+        [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+        newimage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
+    
+    return newimage;
+}
+
++ (UIImage *)thumbnailWithImageWithoutScale:(UIImage *)image size:(CGSize)asize {
+    UIImage *newimage;
+    
+    if (nil == image)
+    {
+        newimage = nil;
+    }
+    else
+    {
+        CGSize oldsize = image.size;
+        CGRect rect;
+        
+        if (asize.width/asize.height > oldsize.width/oldsize.height)
+        {
+            rect.size.width = asize.height*oldsize.width/oldsize.height;
+            rect.size.height = asize.height;
+            rect.origin.x = (asize.width - rect.size.width)/2;
+            rect.origin.y = 0;
+        }
+        else
+        {
+            rect.size.width = asize.width;
+            rect.size.height = asize.width*oldsize.height/oldsize.width;
+            rect.origin.x = 0;
+            rect.origin.y = (asize.height - rect.size.height)/2;
+        }
+        
+        CGFloat scale = [[UIScreen mainScreen] scale];
+        UIGraphicsBeginImageContextWithOptions(asize, NO, scale);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context, [[UIColor clearColor] CGColor]);
+        UIRectFill(CGRectMake(0, 0, asize.width, asize.height));//clear background
+        [image drawInRect:rect];
+        newimage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
+    
+    return newimage;
+}
+
++ (UIImage *)thumbnailWithImage:(UIImage *)image fitInSize:(CGSize)viewsize {
+    // calculate the fitted size
+    CGSize size = [self fitSize:image.size inSize:viewsize];
+    
+    CGFloat scale = [[UIScreen mainScreen] scale];
+    UIGraphicsBeginImageContextWithOptions(viewsize, NO, scale);
+    
+    float dwidth = (viewsize.width - size.width) / 2.0f;
+    float dheight = (viewsize.height - size.height) / 2.0f;
+    
+    CGRect rect = CGRectMake(dwidth, dheight, size.width, size.height);
+    [image drawInRect:rect];
+    
+    UIImage *newimg = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newimg;
+}
+
++ (UIImage *) thumbnailWithImage:(UIImage *)image centerInSize:(CGSize) viewsize {
+    CGSize size = image.size;
+    
+    CGFloat scale = [[UIScreen mainScreen] scale];
+    UIGraphicsBeginImageContextWithOptions(viewsize, NO, scale);
+    float dwidth = (viewsize.width - size.width) / 2.0f;
+    float dheight = (viewsize.height - size.height) / 2.0f;
+    
+    CGRect rect = CGRectMake(dwidth, dheight, size.width, size.height);
+    [image drawInRect:rect];
+    
+    UIImage *newimg = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newimg;
+}
+
++ (UIImage *)thumbnailWithImage:(UIImage *)image fillInSize:(CGSize)viewsize {
+    CGSize size = image.size;
+    
+    CGFloat scalex = viewsize.width / size.width;
+    CGFloat scaley = viewsize.height / size.height;
+    CGFloat scale = MAX(scalex, scaley);
+    
+    CGFloat imageScale = [[UIScreen mainScreen] scale];
+    UIGraphicsBeginImageContextWithOptions(viewsize, NO, imageScale);
+    
+    CGFloat width = size.width * scale;
+    CGFloat height = size.height * scale;
+    
+    float dwidth = ((viewsize.width - width) / 2.0f);
+    float dheight = ((viewsize.height - height) / 2.0f);
+    
+    CGRect rect = CGRectMake(dwidth, dheight, size.width * scale, size.height * scale);
+    [image drawInRect:rect];
+    
+    UIImage *newimg = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newimg;
+}
+
+- (UIImage *)scaleWithRatio:(CGFloat)scaleRatio
+{
+    CGSize newSize = CGSizeMake(self.size.width * scaleRatio, self.size.height * scaleRatio);
+    CGFloat scale = [[UIScreen mainScreen] scale];
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, scale);
+    [self drawInRect:CGRectMake(0, 0, newSize.width , newSize.height)];
+    UIImage *scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return scaledImage;
+}
+
+- (NSString *)image2String {
+    NSData* pictureData = UIImageJPEGRepresentation(self,1.0);
+    NSString* pictureDataString = [pictureData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    return pictureDataString;
+}
+
+- (UIImage *)applyBlurWithRadius:(CGFloat)blurRadius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor maskImage:(UIImage *)maskImage
+{
+    // Check pre-conditions.
+    if (self.size.width < 1 || self.size.height < 1) {
+        NSLog (@"*** error: invalid size: (%.2f x %.2f). Both dimensions must be >= 1: %@", self.size.width, self.size.height, self);
+        return nil;
+    }
+    if (!self.CGImage) {
+        NSLog (@"*** error: image must be backed by a CGImage: %@", self);
+        return nil;
+    }
+    if (maskImage && !maskImage.CGImage) {
+        NSLog (@"*** error: maskImage must be backed by a CGImage: %@", maskImage);
+        return nil;
+    }
+    
+    CGRect imageRect = { CGPointZero, self.size };
+    UIImage *effectImage = self;
+    
+    BOOL hasBlur = blurRadius > __FLT_EPSILON__;
+    BOOL hasSaturationChange = fabs(saturationDeltaFactor - 1.) > __FLT_EPSILON__;
+    if (hasBlur || hasSaturationChange) {
+        UIGraphicsBeginImageContextWithOptions(self.size, NO, [[UIScreen mainScreen] scale]);
+        CGContextRef effectInContext = UIGraphicsGetCurrentContext();
+        CGContextScaleCTM(effectInContext, 1.0, -1.0);
+        CGContextTranslateCTM(effectInContext, 0, -self.size.height);
+        CGContextDrawImage(effectInContext, imageRect, self.CGImage);
+        
+        vImage_Buffer effectInBuffer;
+        effectInBuffer.data     = CGBitmapContextGetData(effectInContext);
+        effectInBuffer.width    = CGBitmapContextGetWidth(effectInContext);
+        effectInBuffer.height   = CGBitmapContextGetHeight(effectInContext);
+        effectInBuffer.rowBytes = CGBitmapContextGetBytesPerRow(effectInContext);
+        
+        UIGraphicsBeginImageContextWithOptions(self.size, NO, [[UIScreen mainScreen] scale]);
+        CGContextRef effectOutContext = UIGraphicsGetCurrentContext();
+        vImage_Buffer effectOutBuffer;
+        effectOutBuffer.data     = CGBitmapContextGetData(effectOutContext);
+        effectOutBuffer.width    = CGBitmapContextGetWidth(effectOutContext);
+        effectOutBuffer.height   = CGBitmapContextGetHeight(effectOutContext);
+        effectOutBuffer.rowBytes = CGBitmapContextGetBytesPerRow(effectOutContext);
+        
+        if (hasBlur) {
+            // A description of how to compute the box kernel width from the Gaussian
+            // radius (aka standard deviation) appears in the SVG spec:
+            // http://www.w3.org/TR/SVG/filters.html#feGaussianBlurElement
+            //
+            // For larger values of 's' (s >= 2.0), an approximation can be used: Three
+            // successive box-blurs build a piece-wise quadratic convolution kernel, which
+            // approximates the Gaussian kernel to within roughly 3%.
+            //
+            // let d = floor(s * 3*sqrt(2*pi)/4 + 0.5)
+            //
+            // ... if d is odd, use three box-blurs of size 'd', centered on the output pixel.
+            //
+            CGFloat inputRadius = blurRadius * [[UIScreen mainScreen] scale];
+            uint32_t radius = floor(inputRadius * 3. * sqrt(2 * M_PI) / 4 + 0.5);
+            if (radius % 2 != 1) {
+                radius += 1; // force radius to be odd so that the three box-blur methodology works.
+            }
+            vImageBoxConvolve_ARGB8888(&effectInBuffer, &effectOutBuffer, NULL, 0, 0, (uint32_t)radius, (uint32_t)radius, 0, kvImageEdgeExtend);
+            vImageBoxConvolve_ARGB8888(&effectOutBuffer, &effectInBuffer, NULL, 0, 0, (uint32_t)radius, (uint32_t)radius, 0, kvImageEdgeExtend);
+            vImageBoxConvolve_ARGB8888(&effectInBuffer, &effectOutBuffer, NULL, 0, 0, (uint32_t)radius, (uint32_t)radius, 0, kvImageEdgeExtend);
+            
+        }
+        BOOL effectImageBuffersAreSwapped = NO;
+        if (hasSaturationChange) {
+            CGFloat s = saturationDeltaFactor;
+            CGFloat floatingPointSaturationMatrix[] = {
+                0.0722 + 0.9278 * s,  0.0722 - 0.0722 * s,  0.0722 - 0.0722 * s,  0,
+                0.7152 - 0.7152 * s,  0.7152 + 0.2848 * s,  0.7152 - 0.7152 * s,  0,
+                0.2126 - 0.2126 * s,  0.2126 - 0.2126 * s,  0.2126 + 0.7873 * s,  0,
+                0,                    0,                    0,  1,
+            };
+            const int32_t divisor = 256;
+            NSUInteger matrixSize = sizeof(floatingPointSaturationMatrix)/sizeof(floatingPointSaturationMatrix[0]);
+            int16_t saturationMatrix[matrixSize];
+            for (NSUInteger i = 0; i < matrixSize; ++i) {
+                saturationMatrix[i] = (int16_t)roundf(floatingPointSaturationMatrix[i] * divisor);
+            }
+            if (hasBlur) {
+                vImageMatrixMultiply_ARGB8888(&effectOutBuffer, &effectInBuffer, saturationMatrix, divisor, NULL, NULL, kvImageNoFlags);
+                effectImageBuffersAreSwapped = YES;
+            }
+            else {
+                vImageMatrixMultiply_ARGB8888(&effectInBuffer, &effectOutBuffer, saturationMatrix, divisor, NULL, NULL, kvImageNoFlags);
+            }
+        }
+        if (!effectImageBuffersAreSwapped)
+            effectImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        if (effectImageBuffersAreSwapped)
+            effectImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
+    
+    // Set up output context.
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, [[UIScreen mainScreen] scale]);
+    CGContextRef outputContext = UIGraphicsGetCurrentContext();
+    CGContextScaleCTM(outputContext, 1.0, -1.0);
+    CGContextTranslateCTM(outputContext, 0, -self.size.height);
+    
+    // Draw base image.
+    CGContextDrawImage(outputContext, imageRect, self.CGImage);
+    
+    // Draw effect image.
+    if (hasBlur) {
+        CGContextSaveGState(outputContext);
+        if (maskImage) {
+            CGContextClipToMask(outputContext, imageRect, maskImage.CGImage);
+        }
+        CGContextDrawImage(outputContext, imageRect, effectImage.CGImage);
+        CGContextRestoreGState(outputContext);
+    }
+    
+    // Add in color tint.
+    if (tintColor) {
+        CGContextSaveGState(outputContext);
+        CGContextSetFillColorWithColor(outputContext, tintColor.CGColor);
+        CGContextFillRect(outputContext, imageRect);
+        CGContextRestoreGState(outputContext);
+    }
+    
+    // Output image is ready.
+    UIImage *outputImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return outputImage;
+}
+
+#pragma mark - Helper
+//计算适合的大小。并保留其原始图片大小
+
++ (CGSize) fitSize: (CGSize)thisSize inSize:(CGSize)aSize
+{
+    CGFloat scale;
+    CGSize newsize = thisSize;
+    
+    if (newsize.height && (newsize.height > aSize.height))
+    {
+        scale = aSize.height / newsize.height;
+        newsize.width *= scale;
+        newsize.height *= scale;
+    }
+    
+    if (newsize.width && (newsize.width >= aSize.width))
+    {
+        scale = aSize.width / newsize.width;
+        newsize.width *= scale;
+        newsize.height *= scale;
+    }
+    
+    return newsize;
 }
 
 @end
